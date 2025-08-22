80a3c59548936c1b63eff53cf4b8be00182259e9
Lee Klopfers <lee.klopfers@karcher.com>
2025-08-22 13:44:28 +0200

    adds components

--- src/src/api/testtest/controllers/testtest.ts
+++
@@ -1,7 +0,0 @@
-/**
- * testtest controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::testtest.testtest');
--- src/src/api/testtest/services/testtest.ts
+++
@@ -1,7 +0,0 @@
-/**
- * testtest service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::testtest.testtest');
